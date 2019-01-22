7e617411ff51a553126fa8de10aa0f7adf4418db
nBow <nbow51@gmail.com>
2019-01-22 00:21:52 -0800

    new buttons/inputs

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,4 @@
+
 // Get references to page elements
 var $searchInput = $("#search-bar");
 var $exampleDescription = $("#example-description");
@@ -6,7 +7,7 @@ var $exampleList = $("#example-list");
 
 // The API object contains methods for each kind of request we'll make
 var API = {
-  search: function($search) {
+  search: function ($search) {
     return $.ajax({
       headers: {
         "Content-Type": "application/json"
@@ -16,13 +17,13 @@ var API = {
       data: JSON.stringify($search)
     });
   },
-  getIngredient: function() {
+  getIngredient: function () {
     return $.ajax({
       url: "api/ingredients",
       type: "GET"
     });
   },
-  deleteIngredient: function(id) {
+  deleteIngredient: function (id) {
     return $.ajax({
       url: "api/ingredients/" + id,
       type: "DELETE"
@@ -34,8 +35,8 @@ var API = {
 function showPassword() {
   var x = document.getElementById("pwInput");
   if (x.type === "password") {
-    x.type= "text";
-  }else {
+    x.type = "text";
+  } else {
     x.type = "password";
   }
 }
@@ -43,30 +44,92 @@ function showPassword() {
 function showPassword2() {
   var x = document.getElementById("pwResult");
   if (x.type === "password") {
-    x.type= "text";
-  }else {
+    x.type = "text";
+  } else {
     x.type = "password";
   }
 }
 
-//Show Create Account Form
-$("#newAccount").click(function(){$("#companyName, #buttons").addClass("hidden");});
-$("#newAccount").click(function(){$("#newAccountForm, #setupAccountTitle").removeClass("hidden");});
+var Hook = function () {
+  this.signIn = $(".signIn");
+  this.signUp = $(".signUp");
+  this.sign = $("#sign");
+  this.button = $("body button");
+  this.first = $("#firstNameInput").val().trim();
+  this.last = $("#lastNameInput").val().trim();
+  this.email = $("#emailInput").val().trim();
+  this.user = $("#userNameInput").val().trim();
+  this.pass = $("#pwInput").val().trim();
+  this.form = $("#newAccountForm");
+  this.main = $(".mainView");
+  this.signUpT = $("#setupAccountTitle")
+  this.signInT = $("#signInTitle")
 
-//Show Sign In Form
-$("#signIn").click(function(){$("#companyName, #buttons").addClass("hidden");});
-$("#signIn").click(function(){$("#signInForm, #signInTitle").removeClass("hidden");});
+  this.show = (arg) => {
+    arg.removeClass("hidden");
+    return arg;
+  };
+  this.hide = (arg) => {
+    arg.addClass("hidden");
+    return arg;
+  };
+  return this;
+};
+
+var hook = new Hook();
+
+hook.button.on("click", function (e) {
+  e.preventDefault();
+  var b = $(this);
+  switch (b.attr("id")) {
+    case "signIn":
+      showForm(true);
+      break;
+    case "newAccount":
+      showForm(false);
+      break;
+    case "sign":
+      if (b.data("form") === 0) {
+        //create account
+      } else {
+        //sign in
+      }
+      break;
+    case "cancelButton":
+      hook.hide(hook.form);
+      hook.show(hook.main);
+      break;
+
+  }
+})
+//form showing funciton true = signIn, false = signUp
+var showForm = function (bool) {
+  hook.show(hook.form);
+  hook.hide(hook.main);
+
+  if (bool) {
+    hook.sign
+      .html("Sign In")
+      .attr("data-form", 1)
+    hook.show(hook.signIn);
+    hook.hide(hook.signUp);
+    hook.hide(hook.signUpT);
+  } else {
+    hook.sign
+      .html("sign Up")
+      .attr("data-form", 0)
+    hook.show(hook.signUp);
+    hook.hide(hook.signInT);
+  }
+};
 
 
-//Show Home page if you click cancel on the create account page
-$("#signInCancelButton").click(function(){$("#companyName, #buttons").removeClass("hidden");});
-$("#signInCancelButton").click(function(){$("#newAccountForm, #setupAccountTitle").addClass("hidden");});
 
 
 // refreshExamples gets new examples from the db and repopulates the list
-var refreshIngredients = function() {
-  API.getIngredient().then(function(data) {
-    var $ingredient = data.map(function(ingredient) {
+var refreshIngredients = function () {
+  API.getIngredient().then(function (data) {
+    var $ingredient = data.map(function (ingredient) {
       var $a = $("<a>")
         .text(ingredient.foodName)
         .attr("href", "/ingredient/" + ingredient.id);
@@ -94,7 +157,7 @@ var refreshIngredients = function() {
 
 // handleFormSubmit is called whenever we submit a new example
 // Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
+var handleFormSubmit = function (event) {
   event.preventDefault();
 
   var search = {
@@ -107,7 +170,7 @@ var handleFormSubmit = function(event) {
     return;
   }
 
-  API.saveExample(example).then(function() {
+  API.saveExample(example).then(function () {
     refreshExamples();
   });
 
@@ -117,16 +180,16 @@ var handleFormSubmit = function(event) {
 
 // handleDeleteBtnClick is called when an example's delete button is clicked
 // Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function() {
+var handleDeleteBtnClick = function () {
   var idToDelete = $(this)
     .parent()
     .attr("data-id");
 
-  API.deleteExample(idToDelete).then(function() {
+  API.deleteExample(idToDelete).then(function () {
     refreshIngredients();
   });
 };
 
 // Add event listeners to the submit and delete buttons
-$submitBtn.on("click", handleFormSubmit);
-$exampleList.on("click", ".delete", handleDeleteBtnClick);
+// $submitBtn.on("click", handleFormSubmit);
+// $exampleList.on("click", ".delete", handleDeleteBtnClick);
