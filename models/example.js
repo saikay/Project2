b2940169a0b2c0efa227dc8e91e948c20a94fe7e
Joshua Moore <jommoore2003@me.com>
2019-01-23 21:36:36 -0800

    merging from Master

--- models/example.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-  var Food = sequelize.define("Food", {
-    foodName: DataTypes.STRING,
-    searchScore: DataTypes.INTEGER
-  });
-  return Food;
-};
